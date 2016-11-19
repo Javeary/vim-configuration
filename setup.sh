bf63625062ec295f5191e553ec421a258e563c56
Mushu.Li <Li>
2016-11-19 17:02:12 +0800

    delete redundant files and modify the path of the remote repository by setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
+echo " It takes time to install, please be patient! "
 if which apt-get >/dev/null; then
 	sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools python-dev git
 elif which yum >/dev/null; then
@@ -12,18 +12,14 @@ if which brew >/dev/null;then
     brew install vim ctags git astyle
 fi
 
-sudo easy_install -ZU autopep8 
+sudo easy_install -ZU autopep8
 sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
 mv -f ~/vim ~/vim_old
-cd ~/ && git clone https://github.com/ma6174/vim.git
+cd ~/ && git clone https://github.com/Javeary/vim-configuration.git
 mv -f ~/.vim ~/.vim_old
-mv -f ~/vim ~/.vim
+mv -f ~/vim-configuration ~/.vim
 mv -f ~/.vimrc ~/.vimrc_old
 mv -f ~/.vim/.vimrc ~/
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-echo "ma6174正在努力为您安装bundle程序" > ma6174
-echo "安装完毕将自动退出" >> ma6174
-echo "请耐心等待" >> ma6174
-vim ma6174 -c "BundleInstall" -c "q" -c "q"
-rm ma6174
-echo "安装完成"
+
+echo "success complete!"
